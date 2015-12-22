daa205e84b57a9398cc6951a4faddf997244214f
BruceBerry <r.pelizzi@gmail.com>
2015-12-22 10:23:15 -0500

    Adapting to new interface

--- test/extension/index.js
+++ test/extension/index.js
@@ -1,68 +1,65 @@
 var prefs = require("sdk/preferences/service");
 var system = require("sdk/system");
+var timers = require("sdk/timers");
 
-var {navigate} = require("addon-navigate");
 
-var input = ["http://www.yahoo.com"]; //, "http://yahoo.com"];
+var {Navigator} = require("addon-navigate");
+
+var input = ["http://localhost/dom-xss/test/examples/dombased.php?sink=eval#alert('xss');"];
+// var input = ["http://www.yahoo.com"]; //, "http://yahoo.com"];
 
 var config = {
-  ignoreFirst: 3,
-  minSamples: 2,
-  close: true
+  wait: 2, // wait for the other extension to start
+  ignoreFirst: 1 // remove first N time samples
 };
 
 function processHalf(h) {
+  if (h.processed)
+    return; // do not process twice
+  h.processed = true;
   h.times = h.times.slice(config.ignoreFirst);
-  if (h.times.length < config.minSamples)
+  if (h.times.length === 0)
     return;
-  console.log(h);
   h.min = Math.min.apply(null, h.times);
   h.max = Math.max.apply(null, h.times);
   var sum = h.times.reduce((acc, cur) => acc + cur, 0);
-  var sumsqr = h.times.reduce((acc, cur) => acc + Math.pow(cur, 2), 0);
   h.avg = Math.round(sum / h.times.length);
-  h.stddev = Math.round(Math.sqrt(sumsqr / h.times.length - Math.pow(h.avg, 2)));
 }
 
 function processSite(s) {
-  console.log("ps", s);
-  processHalf(s.on);
-  processHalf(s.off);
-  if (s.on.avg && s.off.avg)
+  s.on && processHalf(s.on);
+  s.off && processHalf(s.off);
+  if (s.on && s.on.avg && s.off && s.off.avg)
     s.overhead = (s.on.avg - s.off.avg) / s.off.avg * 100;
 }
 
-navigate(input, {
-  times: 8,
-  errors: 1,
-  random: true,
-  doOff: true,
-  timeout: 10,
-  loadDelay: 0
-}, {
-  extraGlobals: function(w, cloner) {
-    w.onerror = function(msg) {
-      console.log("w error", msg);
-    };
-  },
-  beforeOpen: function(site, which) {
-    console.log("open", site, which);
-  },
-  beforeClose: function(site, which, tab, isTimeout) {
-    console.log("close", site, which, isTimeout);
-  },
-  doOff: function() { 
-    prefs.set("security.dxf.rewrite", false);
-  },
-  doOn: function() {
-    prefs.set("security.dxf.rewrite", true);
-  },
-  end: function(sites) {
-    this.doOn(); // reset
-    sites.forEach(processSite);
-    console.log("end", sites);
-    if (config.close) {
-      system.exit(0);
+timers.setTimeout(function() {
+  var n = new Navigator(input, {times: 4}, {
+    extraProperties: function(site) {
+      site.on.attack = false;
+      if (site.off)
+        site.off.attack = false;
+    },
+    extraGlobals: function(w, cloner) {
+      w.onerror = msg => {
+        if (msg.includes("DOM-Based XSS"))
+          this.half.attack = true;
+        else
+          this.half.jsErrors.push(msg); // we could call the original handler
+      };
+    },
+    doOff: function() { 
+      prefs.set("security.dxf.rewrite", false);
+    },
+    doOn: function() {
+      prefs.set("security.dxf.rewrite", true);
+    },
+    end: function() {
+      this.doOn(); // reset
+      this.sites.forEach(processSite);
+      console.log("end", this.sites);
+      // system.exit(0);
     }
-  }
-});
+  });
+  n.start();
+}, config.wait * 1000);
