1d56019e23a063cc34c025501c5212419d6d3db0
BruceBerry <r.pelizzi@gmail.com>
2015-12-24 22:16:31 -0500

    added benign test handler, keep monitoring for localserver

--- test/extension/index.js
+++ test/extension/index.js
@@ -1,6 +1,7 @@
 var prefs = require("sdk/preferences/service");
 var system = require("sdk/system");
 var timers = require("sdk/timers");
+var self = require("sdk/self");
 
 
 var {Navigator} = require("addon-navigate");
@@ -17,17 +18,14 @@ var vectorInput = [
   "http://localhost/dom-xss/test/examples/dombased.php?sink=innerHTML#<img src=\"http://sdfgofgd.net/\" onerror=\"alert(1)\">"
 ];
 
+var benignInput = self.data.load("top5000.txt").split("\n").map(l => "http://www." + l.split(",")[1]);
+benignInput = benignInput.slice(0,2);
 
-var config = {
-  wait: 1, // wait for the other extension to start
-  ignoreFirst: 1 // remove first N time samples
-};
-
-function processHalf(h) {
+function processHalf(h, ignoreFirst=0) {
   if (h.processed)
     return; // do not process twice
   h.processed = true;
-  h.times = h.times.slice(config.ignoreFirst);
+  h.times = h.times.slice(ignoreFirst);
   if (h.times.length === 0)
     return;
   h.min = Math.min.apply(null, h.times);
@@ -57,6 +55,10 @@ var dxfHandler = {
         this.half.jsErrors.push(msg); // we could call the original handler
     };
   },
+  extraPrefs: function() {
+    prefs.set("security.dxf.enforce", true);
+    prefs.set("security.dxf.minify", false);
+  },
   doOff: function() { 
     prefs.set("security.dxf.rewrite", false);
   },
@@ -64,7 +66,6 @@ var dxfHandler = {
     prefs.set("security.dxf.rewrite", true);
   },
   end: function() {
-    this.doOn(); // reset
     this.sites.forEach(processSite);
     console.log("end", this.sites);
     // system.exit(0);
@@ -75,12 +76,33 @@ var vectorHandler = {
   end: function() {
     dxfHandler.end.call(this);
     this.sites.forEach(s => { if (!s.on.attack) throw s.url });
-    console.log("All sites are attacks")
+    console.log("All attacks detected");
   }
-}
+};
 vectorHandler.__proto__ = dxfHandler;
 
+var benignHandler = {
+  end: function() {
+    dxfHandler.end.call(this);
+    this.sites.forEach(function(site) {
+      // check attack = false && msgs match
+      if (site.on.attack === true || !msgMatch(site.on.jsErrors, site.off.jsErrors)) {
+        debugger;
+        console.log("Problem on site", site.url, site);
+      }
+    });
+  }
+};
+benignHandler.__proto__ = dxfHandler;
+
+var msgMatch = function(a, b) {
+  a.sort();
+  b.sort();
+  return a.toString() === b.toString();
+}
+
 timers.setTimeout(function() {
-  var n = new Navigator(vectorInput, {times: 1}, vectorHandler);
+  // var n = new Navigator(vectorInput, {times: 1}, vectorHandler);
+  var n = new Navigator(benignInput, {times: 1, abTesting: true}, benignHandler);
   n.start();
-}, config.wait * 1000);
+}, 0.5 * 1000);
